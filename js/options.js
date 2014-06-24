60cf6da7ae6cd8c4874f6a403ee5b81d4fc707e3
unknown <bill.scott@gmail.com>
2014-06-24 12:58:21 -0700

    Working on splash screen

--- js/options.js
+++ js/options.js
@@ -9,9 +9,28 @@ EG3.Options = function() {
 EG3.Options.prototype = {
   preload: function() {
     console.log("Options.preload");
-    this.gameLogo = this.game.add.sprite(this.game.width/2-120, 100, 'gameLogo');
-    this.startButton = this.game.add.button(this.game.width/2, 300, 'startButton', this.startClicked, this);
+    this.game.add.sprite(0,0,"splash");
+//    this.gameLogo = this.game.add.sprite(this.game.width/2-120, 100, 'gameLogo');
+    yVal = (this.game.height/6)*5;
+    this.startButton = this.game.add.button(this.game.width/2, yVal, 'startButton', this.startClicked, this);
     this.startButton.anchor.setTo(0.5,0.5);
+
+    var cenX = (this.game.world.width/2);
+    var cenY = (this.game.world.height/3);
+
+    this.playerBody = this.game.add.sprite(cenX, cenY, 'playerBody');
+    this.playerBody.anchor.setTo(0.5, 0.5);
+    this.blankEye = this.game.add.sprite(cenX, cenY, 'blankEye');
+    this.blankEye.anchor.setTo(0.5, 0.5);
+    this.pupil = this.game.add.sprite(cenX, cenY, 'pupil');
+    this.pupil.anchor.setTo(0.5, 0.5);
+    this.mouth = this.game.add.sprite(cenX, cenY, 'mouth');
+    this.mouth.anchor.setTo(0.5, 0.5);
+    var t1 = this.game.add.tween(this.playerBody.scale).to({x:4, y:4}, 1000, Phaser.Easing.Quadratic.In, true);
+    var t1 = this.game.add.tween(this.blankEye.scale).to({x:4, y:4}, 1000, Phaser.Easing.Quadratic.In, true);
+    var t1 = this.game.add.tween(this.pupil.scale).to({x:4, y:4}, 1000, Phaser.Easing.Quadratic.In, true);
+    var t1 = this.game.add.tween(this.mouth.scale).to({x:4, y:4}, 1000, Phaser.Easing.Quadratic.In, true);
+
   },
   create: function() {
     console.log("Options.create");
--- js/preload.js
+++ js/preload.js
@@ -14,11 +14,16 @@ EG3.Preload.prototype = {
     //this.preloadSprite = this.add.sprite(this.game.world.width/2,this.game.world.height/2, 'preloadeImage');
 
     this.game.load.image('bg', 'assets/bg2.png');
+    this.game.load.image('splash', 'assets/splashBackgroundSmall.png');
     this.game.load.image('gameLogo', 'assets/someGame.png');
     this.game.load.image('startButton', 'assets/startButton.png');
-    this.game.load.image('greenBall', 'assets/greenBall.png');    
+    this.game.load.image('greenBall', 'assets/greenBall.png');
     this.game.load.image('playerBody', 'assets/playerBody.png');
-    this.game.load.image('playerEye', 'assets/playerEye.png');        
+    this.game.load.image('playerEye', 'assets/playerEye.png');
+    this.game.load.image('deadEye', 'assets/deadEye.png');
+    this.game.load.image('blankEye', 'assets/blankEye.png');
+    this.game.load.image('mouth', 'assets/mouth.png');
+    this.game.load.image('pupil', 'assets/pupil.png');
     //Load any audio here
 
     this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
